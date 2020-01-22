e5c7ae6995f293491e3631f9ea58a83f7faff8bf
emmakravets <veseladove@gmail.com>
2020-01-22 21:37:16 +0300

    Changes renderStatistics and adds new constants

--- js/stat.js
+++ js/stat.js
@@ -4,60 +4,64 @@ var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
-var GAP = 10;
-var TEXT_X = 140;
-var TEXT_Y = 250;
-var TEXT_GAP = 20;
+var CLOUD_COLOR = '#ffffff';
+var CLOUD_SHADOW = 'rgba(0, 0, 0, 0.7)';
+var CLOUD_GAP = 10;
+var TEXT_X = 20;
+var TEXT_Y = 30;
+var TEXT_COLOR = '#000000';
+var TEXT_FONT = '16px PT Mono';
+var TEXT_BASELINE = 'hanging';
 var BAR_Y = 90;
 var BAR_STEP = 50;
 var BAR_WIDTH = 40;
-var barHeight = 150;
+var BAR_HEIGHT = 150;
+var BAR_TEXT_X = 140;
+var BAR_TEXT_Y = 250;
+var BAR_COLOR = 'rgba(255, 0, 0, 1)';
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 // находим макс. время
-var getMaxElement = function (arr) {
-  var maxElement = arr[0];
+var getMaxItem = function (arr) {
+  var maxItem = arr[0];
 
   for (var i = 0; i < arr.length; i++) {
-    if (arr[i] > maxElement) {
-      maxElement = arr[i];
+    if (arr[i] > maxItem) {
+      maxItem = arr[i];
     }
   }
-  return maxElement;
+  return maxItem;
 };
 
 window.renderStatistics = function (ctx, players, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
+  renderCloud(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, CLOUD_SHADOW);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
 
-  ctx.fillStyle = '#000000';
-  ctx.font = '16px PT Mono';
-  ctx.textBaseline = 'hanging';
-  ctx.fillText('Ура вы победили!', CLOUD_X + TEXT_GAP, 30);
-  ctx.fillText('Список результатов:', CLOUD_X + TEXT_GAP, 30 + TEXT_GAP);
+  ctx.fillStyle = TEXT_COLOR;
+  ctx.font = TEXT_FONT;
+  ctx.textBaseline = TEXT_BASELINE;
+  ctx.fillText('Ура вы победили!', CLOUD_X + TEXT_X, TEXT_Y);
+  ctx.fillText('Список результатов:', CLOUD_X + TEXT_X, TEXT_Y + TEXT_X);
 
-  // var players = ['Вы', 'Кекс', 'Сашка', 'Петька'];
-  var maxTime = getMaxElement(times);
+  var maxTime = getMaxItem(times);
 
   for (var i = 0; i < players.length; i++) {
-    // x = (barHeight * bar[i]) / maxBar;
-    ctx.fillText(players[i], TEXT_X + (BAR_WIDTH + BAR_STEP) * i, TEXT_Y);
-    ctx.fillRect(TEXT_X + (BAR_WIDTH + BAR_STEP) * i, (BAR_Y + barHeight) - (barHeight * times[i]) / maxTime, BAR_WIDTH, (barHeight * times[i]) / maxTime);
+    ctx.fillStyle = TEXT_COLOR;
+
+    var coordinateLeft = BAR_TEXT_X + (BAR_WIDTH + BAR_STEP) * i;
+    var barHeight = (BAR_HEIGHT * times[i]) / maxTime;
+    ctx.fillText(players[i], coordinateLeft, BAR_TEXT_Y);
+    ctx.fillText(Math.floor(times[i]), coordinateLeft, (BAR_TEXT_Y - TEXT_Y) - barHeight);
+
+    if (players[i] === 'Вы') {
+      ctx.fillStyle = BAR_COLOR;
+    } else {
+      ctx.fillStyle = 'hsl(240, ' + Math.floor(Math.random() * 100) + '%, 50%)';
+    }
+
+    ctx.fillRect(coordinateLeft, (BAR_Y + BAR_HEIGHT) - barHeight, BAR_WIDTH, barHeight);
   }
-
-  /* ctx.fillText('Вы', TEXT_X + (BAR_WIDTH + BAR_STEP) * 0, TEXT_Y);
-  ctx.fillRect(TEXT_X + (BAR_WIDTH + BAR_STEP) * 0, BAR_Y, BAR_WIDTH, barHeight);
-
-  ctx.fillText('Кекс', TEXT_X + (BAR_WIDTH + BAR_STEP) * 1, TEXT_Y);
-  ctx.fillRect(TEXT_X + (BAR_WIDTH + BAR_STEP) * 1, BAR_Y, BAR_WIDTH, barHeight);
-
-  ctx.fillText('Сашка', TEXT_X + (BAR_WIDTH + BAR_STEP) * 2, TEXT_Y);
-  ctx.fillRect(TEXT_X + (BAR_WIDTH + BAR_STEP) * 2, BAR_Y, BAR_WIDTH, barHeight);
-
-  ctx.fillText('Петька', TEXT_X + (BAR_WIDTH + BAR_STEP) * 3, TEXT_Y);
-  ctx.fillRect(TEXT_X + (BAR_WIDTH + BAR_STEP) * 3, BAR_Y, BAR_WIDTH, barHeight);
-  */
 };
