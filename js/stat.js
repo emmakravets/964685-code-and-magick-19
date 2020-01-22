eec079ea0ead2834010981f5e51c61491ec98ee8
emmakravets <veseladove@gmail.com>
2020-01-22 23:41:33 +0300

    Adds generateRandomColor, renderMessages funcs

--- js/stat.js
+++ js/stat.js
@@ -7,11 +7,13 @@ var CLOUD_Y = 10;
 var CLOUD_COLOR = '#ffffff';
 var CLOUD_SHADOW = 'rgba(0, 0, 0, 0.7)';
 var CLOUD_GAP = 10;
-var TEXT_X = 20;
+
+var TEXT_GAP = 20;
 var TEXT_Y = 30;
 var TEXT_COLOR = '#000000';
 var TEXT_FONT = '16px PT Mono';
 var TEXT_BASELINE = 'hanging';
+
 var BAR_Y = 90;
 var BAR_STEP = 50;
 var BAR_WIDTH = 40;
@@ -20,10 +22,26 @@ var BAR_TEXT_X = 140;
 var BAR_TEXT_Y = 250;
 var BAR_COLOR = 'rgba(255, 0, 0, 1)';
 
+var MESSAGES_TEXT = ['Ура вы победили!', 'Список результатов:'];
+
+var renderMessages = function (ctx, messagesArr, font, fontColor, baseline, x, y, lineHeight) {
+  for (var i = 0; i < messagesArr.length; i++) {
+    ctx.fillStyle = fontColor;
+    ctx.font = font;
+    ctx.textBaseline = baseline;
+    ctx.fillText(messagesArr[i], x, y + lineHeight * i);
+  }
+};
+
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
+
+var generateRandomColor = function () {
+  var randomColor = 'hsl(240, ' + Math.floor(Math.random() * 100) + '%, 50%)';
+  return randomColor;
+}
 // находим макс. время
 var getMaxItem = function (arr) {
   var maxItem = arr[0];
@@ -40,27 +58,21 @@ window.renderStatistics = function (ctx, players, times) {
   renderCloud(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, CLOUD_SHADOW);
   renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
 
-  ctx.fillStyle = TEXT_COLOR;
-  ctx.font = TEXT_FONT;
-  ctx.textBaseline = TEXT_BASELINE;
-  ctx.fillText('Ура вы победили!', CLOUD_X + TEXT_X, TEXT_Y);
-  ctx.fillText('Список результатов:', CLOUD_X + TEXT_X, TEXT_Y + TEXT_X);
+  renderMessages(ctx, MESSAGES_TEXT, TEXT_FONT, TEXT_COLOR, TEXT_BASELINE, CLOUD_X + TEXT_GAP, CLOUD_Y + TEXT_GAP, TEXT_GAP);
 
   var maxTime = getMaxItem(times);
+  var barHeight;
+  var coordinateLeft;
 
   for (var i = 0; i < players.length; i++) {
     ctx.fillStyle = TEXT_COLOR;
 
-    var coordinateLeft = BAR_TEXT_X + (BAR_WIDTH + BAR_STEP) * i;
-    var barHeight = (BAR_HEIGHT * times[i]) / maxTime;
+    barHeight = (BAR_HEIGHT * times[i]) / maxTime;
+    coordinateLeft = BAR_TEXT_X + (BAR_WIDTH + BAR_STEP) * i;
     ctx.fillText(players[i], coordinateLeft, BAR_TEXT_Y);
     ctx.fillText(Math.floor(times[i]), coordinateLeft, (BAR_TEXT_Y - TEXT_Y) - barHeight);
 
-    if (players[i] === 'Вы') {
-      ctx.fillStyle = BAR_COLOR;
-    } else {
-      ctx.fillStyle = 'hsl(240, ' + Math.floor(Math.random() * 100) + '%, 50%)';
-    }
+    ctx.fillStyle = players[i] === 'Вы' ? BAR_COLOR : generateRandomColor();
 
     ctx.fillRect(coordinateLeft, (BAR_Y + BAR_HEIGHT) - barHeight, BAR_WIDTH, barHeight);
   }
